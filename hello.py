62f5cdca2022755d32e3c127986eff4ba27fb011
AURORAQWY <aurora.qwy@gmail.com>
2019-11-19 15:12:50 +1100

    initial commit

--- hello.py
+++ hello.py
@@ -5,29 +5,67 @@ from flask import redirect
 from flask import abort
 from flask import render_template
 from flask_bootstrap import Bootstrap
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return render_template('base.html')
-    #return '<h1>Hello World!</h1>'
-    #return '<h1>Bad Request</h1>', 400
-    #user_agent = request.headers.get('User-Agent')
-    #return '<p>Your browser is %s</p>' % user_agent
-    #user_agent = request.headers.get('User-Agent')
-    # return '<p>Your browser is %s</p>' % user_agent
-    # return '<h1>Hello World!</h1>'
-    # return '<h1>Bad Request.</h1>', 400
-    
-    # response = make_response('<h1>This document carries a cookie!</h1>')
-    # response.set_cookie('answer', '42')
-    # return response
-    # return redirect('http://www.google.com')
+    # -*- coding: utf-8 -*-
 
-@app.route('/user/<name>')
+
+    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+    df = pd.read_csv(
+        'https://gist.githubusercontent.com/chriddyp/' +
+        '5d1ea79569ed194d432e56108a04d188/raw/' +
+        'a9f9e8076b837d541398e999dcbac2b2826a81f8/' +
+        'gdp-life-exp-2007.csv')
+
+    app.layout = html.Div([
+        dcc.Graph(
+            id='life-exp-vs-gdp',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=df[df['continent'] == i]['gdp per capita'],
+                        y=df[df['continent'] == i]['life expectancy'],
+                        text=df[df['continent'] == i]['country'],
+                        mode='markers',
+                        opacity=0.7,
+                        marker={
+                            'size': 15,
+                            'line': {'width': 0.5, 'color': 'white'}
+                        },
+                        name=i
+                    ) for i in df.continent.unique()
+                ],
+                'layout': go.Layout(
+                    xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+                    yaxis={'title': 'Life Expectancy'},
+                    margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest'
+                )
+            }
+        )
+    ])
+
+
+
+@app.route('/user1/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+# @app.route('/analysis')
+# def analysis():
+#     return "hello"
 # @app.route('/user/<name>')
 # def user(name):
 #     return 'hello, %s!</h1>' % name
