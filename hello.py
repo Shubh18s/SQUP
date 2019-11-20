45879f3467319c2d3d65394b7ceb9ef3119fd8e0
AURORAQWY <aurora.qwy@gmail.com>
2019-11-20 18:36:24 +1100

    initial commit

--- hello.py
+++ hello.py
@@ -1,121 +1,64 @@
+
 from flask import Flask
 from flask import request
-from flask import make_response
-from flask import redirect
-from flask import abort
 from flask import render_template
 from flask_bootstrap import Bootstrap
+import plotly
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import json
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import pandas as pd
-import plotly.graph_objs as go
+#from dashapp import server as application
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app1 = dash.Dash(
+           __name__,
+           server=app,
+
+           )
+app1.layout = html.Div(children=[
+           html.H1(children='Hello Dash'),
+           html.Div(children='''
+                    Dash: A web application framework for Python.
+                    '''),
+                    dcc.Graph(
+                            id='example-graph',
+                            figure={
+                                    'data': [
+                                            {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                                            {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                                            ],
+                                            'layout': {
+                                                    'title': 'Dash Data Visualization'
+                                                    }
+                                            }
+                                            )
+        ])
 
 @app.route('/')
 def index():
-    return render_template('base.html')
-    #return '<h1>Hello World!</h1>'
-    #return '<h1>Bad Request</h1>', 400
-    #user_agent = request.headers.get('User-Agent')
-    #return '<p>Your browser is %s</p>' % user_agent
-    #user_agent = request.headers.get('User-Agent')
-    # return '<p>Your browser is %s</p>' % user_agent
-    # return '<h1>Hello World!</h1>'
-    # return '<h1>Bad Request.</h1>', 400
-    
-    # response = make_response('<h1>This document carries a cookie!</h1>')
-    # response.set_cookie('answer', '42')
-    # return response
-    # return redirect('http://www.google.com')
+    return render_template('home.html')
 
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-    df = pd.read_csv(
-        'https://gist.githubusercontent.com/chriddyp/' +
-        '5d1ea79569ed194d432e56108a04d188/raw/' +
-        'a9f9e8076b837d541398e999dcbac2b2826a81f8/' +
-        'gdp-life-exp-2007.csv')
-
-    app.layout = html.Div([
-        dcc.Graph(
-            id='life-exp-vs-gdp',
-            figure={
-                'data': [
-                    go.Scatter(
-                        x=df[df['continent'] == i]['gdp per capita'],
-                        y=df[df['continent'] == i]['life expectancy'],
-                        text=df[df['continent'] == i]['country'],
-                        mode='markers',
-                        opacity=0.7,
-                        marker={
-                            'size': 15,
-                            'line': {'width': 0.5, 'color': 'white'}
-                        },
-                        name=i
-                    ) for i in df.continent.unique()
-                ],
-                'layout': go.Layout(
-                    xaxis={'type': 'log', 'title': 'GDP Per Capita'},
-                    yaxis={'title': 'Life Expectancy'},
-                    margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                    legend={'x': 0, 'y': 1},
-                    hovermode='closest'
-                )
-            }
-        )
-    ])
-
-
-
-@app.route('/user1/<name>')
+@app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
 
-# @app.route('/analysis')
-# def analysis():
-#     return "hello"
-# @app.route('/user/<name>')
-# def user(name):
-#     return 'hello, %s!</h1>' % name
-
-# @app.route('/user/<id>')
-# def get_user(id):
-#     user = load_user(id)
-    
-#     if not user:
-#         abort(404)
-#     return '<h1>Hello, %s</h1>' % user.name
-
 @app.route('/analysis')
-def analyse():
-    external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-    app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-    app.layout = html.Div(children=[
-        html.H1(children='Hello Dash'),
+def analysis():
+    x1 = request.args.get('x1')
+    y1 = request.args.get('y1')
+    x2 = request.args.get('x2')
+    y2 = request.args.get('y2')
 
-        html.Div(children='''
-            Dash: A web application framework for Python.
-        '''),
+    return render_template('analysis.html', x1=x1, x2=x2, y1=y1, y2=y2,
+                           w =request.args.get('w'), h = request.args.get('h'),
+                           name = app1.layout)
 
-        dcc.Graph(
-            id='example-graph',
-            figure={
-                'data': [
-                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                ],
-                'layout': {
-                    'title': 'Dash Data Visualization'
-                }
-            }
-        )
-    ])
-    #return render_template('analysis.html', name=app)
-    return app.index()
+
 
 
 @app.errorhandler(404)
--- justTry.py
+++ justTry.py
@@ -1,8 +1,38 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 19 01:37:24 2019
+from flask import Flask, render_template
+import plotly
+import plotly.graph_objs as go
 
-@author: shubhdeep
-"""
+import pandas as pd
+import numpy as np
+import json
 
-print("Hello world!")
+app = Flask(__name__)
+
+
+@app.route('/test')
+def index():
+    bar = create_plot()
+    return render_template('test_index.html', plot=bar)
+
+def create_plot():
+
+
+    N = 40
+    x = np.linspace(0, 1, N)
+    y = np.random.randn(N)
+    df = pd.DataFrame({'x': x, 'y': y}) # creating a sample dataframe
+
+
+    data = [
+        go.Bar(
+            x=df['x'], # assign x as the dataframe column 'x'
+            y=df['y']
+        )
+    ]
+
+    graphJSON = json.dumps(data, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return graphJSON
+
+if __name__ == '__main__':
+    app.run()
